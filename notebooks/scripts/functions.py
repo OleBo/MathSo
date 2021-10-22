1f9fa0492d467c0bb0479768c503eee7723ae777
Olaf Bochmann <olaf.bochmann@gmail.com>
2021-10-22 17:57:26 +0200

    session 2 update

--- notebooks/scripts/functions.py
+++ notebooks/scripts/functions.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 def interest1(b, p, n):
     """
     INTEREST1(b, p, n) computes the new balance after n years for an initial
@@ -13,9 +14,30 @@ def gauss3(x, mu, sigma):
     """
     return math.exp(-1/2*((x - mu)/sigma)**2)/(sigma*math.sqrt(2*math.pi))
 
-def gauss4(x, mu, sigma):
+def gauss4(x_arr, mu, sigma):
     """
     GAUSS4 Evaluates Gaussian normal distribution with mean mu and standard
-    deviation sigma at x, where x may be a scalar or an array
-    """    
-    return math.exp(-1/2*((x - mu)/sigma)**2)/(sigma*math.sqrt(2*math.pi))
+    deviation sigma at x, where x may be an array
+    """
+    fd = []
+    for x in x_arr:
+            a=1/2*((x - mu)/sigma)**2
+            e=math.exp(-a)
+            fd.append((1/(sigma*math.sqrt(2*math.pi))) * e)
+    return np.array(fd)
+    
+def gauss5(x, mu, sigma):
+    """
+    GAUSS5 Evaluates Gaussian normal distribution with mean mu and standard
+    deviation sigma at x, where x may be an array
+    """
+    return math.exp(-1/2*(np.linalg.matrix_power((x - mu)/sigma),2))/(sigma*math.sqrt(2*math.pi))
+
+def angle(a, b):
+    """
+    ANGLE computes the angle between two vectors a and b in R^2
+    """
+    adotb = np.sum(a*b)            # or np.dot(a,b)
+    norma = np.sqrt(np.sum(a*a));  # or np.linalg.norm(a)
+    normb = np.sqrt(np.sum(b*b));  # or np.linalg.norm(b)
+    return np.arccos(adotb/(norma*normb)); 
